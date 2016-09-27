711fc2af9e6996d5b06a5aa6c108ae733fa3eff7
Derek Weber <derek.weber@dsto.defence.gov.au>
2016-09-27 17:20:50 +0930

    Finished main functionality - needs formatting and documenting

--- src/main/java/au/org/dcw/twitter/ingest/TwitterProfilesRetrieverApp.java
+++ src/main/java/au/org/dcw/twitter/ingest/TwitterProfilesRetrieverApp.java
@@ -15,10 +15,17 @@
  */
 package au.org.dcw.twitter.ingest;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Date;
 import java.util.List;
+import java.util.Properties;
 import java.util.stream.Collectors;
 
 import org.apache.commons.cli.BasicParser;
@@ -29,6 +36,22 @@ import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Lists;
+
+import twitter4j.RateLimitStatus;
+import twitter4j.RateLimitStatusEvent;
+import twitter4j.RateLimitStatusListener;
+import twitter4j.ResponseList;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.TwitterObjectFactory;
+import twitter4j.User;
+import twitter4j.conf.Configuration;
+import twitter4j.conf.ConfigurationBuilder;
 
 /**
  * Application that collects the profiles of Twitter users.
@@ -43,31 +66,27 @@ import org.slf4j.LoggerFactory;
 public final class TwitterProfilesRetrieverApp {
     private static Logger LOG = LoggerFactory.getLogger(TwitterProfilesRetrieverApp.class);
     private static final Options OPTIONS = new Options();
+    private static final int TWITTER_PROFILE_FETCH_BATCH_SIZE = 100;
     static {
         OPTIONS.addOption("i", "ids-file", true, "File of Twitter screen names");
         OPTIONS.addOption("o", "output-directory", true, "Directory to which to write profiles (default: ./profiles)");
+        OPTIONS.addOption("c", "credentials", true, "File of Twitter credentials (default: ./twitter.properties)");
+        OPTIONS.addOption("d", "debug", false, "Turn on debugging information (default: false)");
         OPTIONS.addOption("?", "help", false, "Ask for help with using this tool.");
     }
 
-    private static String screenNamesFile = null;
-    private static List<String> screenNames = null;
-    private static String outputDir = "./profiles";
+    private String screenNamesFile = null;
+    private List<String> screenNames = null;
+    private String outputDir = "./profiles";
+    private String credentialsFile = "./twitter.properties";
+    private boolean debug = false;
 
-    /**
-     * Parses the provided command line arguments and sets class fields accordingly.
-     *
-     * @param args The arguments provided to the app on the commandline.
-     */
-    private static void parseArgs(String[] args) {
-        CommandLineParser parser = new BasicParser();
-        try {
-            CommandLine cmd = parser.parse(OPTIONS, args);
-            if (cmd.hasOption('i')) screenNamesFile = cmd.getOptionValue('i');
-            if (cmd.hasOption('o')) outputDir = cmd.getOptionValue('o');
-            if (cmd.hasOption('h')) printUsageAndExit();
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
+
+    public TwitterProfilesRetrieverApp(String screenNamesFile, String outputDir, String credentialsFile, boolean debug) {
+        this.screenNamesFile = screenNamesFile;
+        this.outputDir = outputDir;
+        this.credentialsFile = credentialsFile;
+        this.debug = debug;
     }
 
     /**
@@ -78,35 +97,220 @@ public final class TwitterProfilesRetrieverApp {
         System.exit(0);
     }
 
-    /**
-     * Checks whether enough fields have been set to successfully run.
-     */
-    private static void checkConfig() {
+    public static void main(String[] args) throws IOException {
+        CommandLineParser parser = new BasicParser();
+        String screenNamesFile = null;
+        String outputDir = "./output";
+        String credentialsFile = "./twitter.properties";
+        boolean debug = false;
+        try {
+            CommandLine cmd = parser.parse(OPTIONS, args);
+            if (cmd.hasOption('i')) screenNamesFile = cmd.getOptionValue('i');
+            if (cmd.hasOption('o')) outputDir = cmd.getOptionValue('o');
+            if (cmd.hasOption('c')) credentialsFile = cmd.getOptionValue('c');
+            if (cmd.hasOption('d')) debug = true;
+            if (cmd.hasOption('h')) printUsageAndExit();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        // check config
         if (screenNamesFile == null) {
             printUsageAndExit();
         }
+
+        new TwitterProfilesRetrieverApp(screenNamesFile, outputDir, credentialsFile, debug).run();
     }
 
-    public static void main(String[] args) throws IOException {
-        parseArgs(args);
-        checkConfig();
+    public void run() throws IOException {
 
         LOG.info("Collecting profiles");
-        LOG.info("  file of Twitter screen names: " + screenNamesFile);
+        LOG.info("  Twitter screen names: " + screenNamesFile);
         LOG.info("  output directory: " + outputDir);
 
         List<String> readAllLines = loadScreenNames();
         screenNames = readAllLines;
 
         LOG.info("Read {} screen names", screenNames.size());
+
+        if (!Files.exists(Paths.get(outputDir))) {
+            LOG.info("Creating output directory {}", outputDir);
+            Paths.get(outputDir).toFile().mkdirs();
+        }
+
+        final Configuration config = TwitterProfilesRetrieverApp.buildTwitterConfiguration(credentialsFile, debug);
+        Twitter twitter = new TwitterFactory(config).getInstance();
+        twitter.addRateLimitStatusListener(rateLimitStatusListener);
+        ObjectMapper json = new ObjectMapper();
+
+        for (List<String> batch : Lists.partition(screenNames, TWITTER_PROFILE_FETCH_BATCH_SIZE)) {
+
+            try {
+                LOG.info("Looking up {} users' profiles", batch.size());
+
+                // ask Twitter for profiles
+                ResponseList<User> profiles = twitter.lookupUsers(batch.toArray(new String[batch.size()]));
+
+                // extract the raw JSON
+                String rawJsonProfiles = TwitterObjectFactory.getRawJSON(profiles);
+
+                // traverse the raw JSON (rather than the Twitter4j structures)
+                JsonNode profilesJsonNode = json.readTree(rawJsonProfiles);
+                profilesJsonNode.forEach (profileNode ->  {
+
+                    // extract vars for logging and filename creation
+                    String profileId = profileNode.get("id_str").asText();
+                    String screenName = profileNode.get("screen_name").asText();
+
+                    String fileName = outputDir + "/profile-" + profileId + ".json";
+
+                    LOG.info("Profile @{} {} -> {}", screenName, profileId, fileName);
+                    try {
+                        saveJSON(profileNode.toString(), fileName);
+                    } catch (IOException e) {
+                        LOG.warn("Failed to write to {}", fileName, e);
+                    }
+                });
+
+            } catch (TwitterException e) {
+                LOG.warn("Failed to communicate with Twitter", e);
+            }
+        }
     }
 
-    private static List<String> loadScreenNames() throws IOException {
+
+    /**
+     * Writes the given {@code rawJSON} {@link String} to the specified file.
+     *
+     * @param rawJSON  the JSON String to persist
+     * @param fileName the file (including path) to which to write the JSON
+     * @throws IOException if there's a problem writing to the specified file
+     */
+    private static void saveJSON(String rawJSON, String fileName) throws IOException {
+        try (FileOutputStream fos = new FileOutputStream(fileName);
+             OutputStreamWriter osw = new OutputStreamWriter(fos, "UTF-8");
+             BufferedWriter bw = new BufferedWriter(osw)) {
+            bw.write(rawJSON);
+            bw.flush();
+        }
+    }
+
+    private List<String> loadScreenNames() throws IOException {
         return Files.readAllLines(Paths.get(screenNamesFile)).stream()
             .map(l -> l.trim())
             .filter(l -> l.length() > 0 && ! l.startsWith("#"))
             .map(sn -> (sn.startsWith("@") ? sn.substring(1): sn))
             .collect(Collectors.toList());
+    }
+
+    /**
+     * Listener to pay attention to when the Twitter's rate limit is being approached or breached.
+     */
+    RateLimitStatusListener rateLimitStatusListener = new RateLimitStatusListener() {
+        @Override
+        public void onRateLimitStatus(RateLimitStatusEvent event) {
+            pauseIfNecessary(event.getRateLimitStatus());
+        }
+
+        @Override
+        public void onRateLimitReached(RateLimitStatusEvent event) {
+            pauseIfNecessary(event.getRateLimitStatus());
+        }
+    };
+
+    /**
+     * If the provided {@link RateLimitStatus} indicates that we are about to break the rate
+     * limit, in terms of number of calls or time window, then sleep for the rest of the period.
+     *
+     * @param status The current status of the our calls to Twitter
+     */
+    protected void pauseIfNecessary(RateLimitStatus status) {
+        if (status == null)
+            return;
+
+        int secondsUntilReset = status.getSecondsUntilReset();
+        int callsRemaining = status.getRemaining();
+        if (secondsUntilReset < 10 || callsRemaining < 10) {
+            int untilReset = status.getSecondsUntilReset() + 5;
+            System.out.println("Rate limit reached. Waiting " + untilReset + " seconds starting at " + new Date() + "...");
+            try {
+                Thread.sleep(untilReset * 1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Resuming...");
+        }
+    }
+
+    /**
+     * Builds the {@link Configuration} object with which to connect to Twitter,
+     * including credentials and proxy information if it's specified.
+     *
+     * @return a Twitter4j {@link Configuration} object
+     * @throws IOException if there's an error loading the application's
+     *         {@link #credentialsFile}.
+     */
+    private static Configuration buildTwitterConfiguration(String credentialsFile, boolean debug) throws IOException {
+        // TODO find a better name than credentials, given it might contain
+        // proxy info
+        final Properties credentials = loadCredentials(credentialsFile);
+
+        final ConfigurationBuilder conf = new ConfigurationBuilder();
+        conf.setJSONStoreEnabled(true).setDebugEnabled(debug)
+            .setOAuthConsumerKey(credentials.getProperty("oauth.consumerKey"))
+            .setOAuthConsumerSecret(credentials.getProperty("oauth.consumerSecret"))
+            .setOAuthAccessToken(credentials.getProperty("oauth.accessToken"))
+            .setOAuthAccessTokenSecret(credentials.getProperty("oauth.accessTokenSecret"));
+
+        final Properties proxies = loadProxyProperties();
+        if (proxies.containsKey("http.proxyHost")) {
+            conf.setHttpProxyHost(proxies.getProperty("http.proxyHost"))
+                .setHttpProxyPort(Integer.parseInt(proxies.getProperty("http.proxyPort")))
+                .setHttpProxyUser(proxies.getProperty("http.proxyUser"))
+                .setHttpProxyPassword(proxies.getProperty("http.proxyPassword"));
+        }
+
+        return conf.build();
+    }
+
+    /**
+     * Loads the given {@code credentialsFile} from disk.
+     *
+     * @param credentialsFile the properties file with the Twitter credentials
+     *        in it
+     * @return A {@link Properties} map with the contents of credentialsFile
+     * @throws IOException if there's a problem reading the credentialsFile.
+     */
+    private static Properties loadCredentials(final String credentialsFile) throws IOException {
+        final Properties properties = new Properties();
+        properties.load(Files.newBufferedReader(Paths.get(credentialsFile)));
+        return properties;
+    }
+
+    /**
+     * Loads proxy properties from {@code ./proxy.properties} and, if a password
+     * is not supplied, asks for it in the console.
+     *
+     * @return A Properties instance filled with proxy information.
+     */
+    private static Properties loadProxyProperties() {
+        final Properties properties = new Properties();
+        final String proxyFile = "./proxy.properties";
+        if (new File(proxyFile).exists()) {
+            boolean success = true;
+            try (Reader fileReader = Files.newBufferedReader(Paths.get(proxyFile))) {
+                properties.load(fileReader);
+            } catch (final IOException e) {
+                System.err.printf("Attempted and failed to load %s: %s\n", proxyFile, e.getMessage());
+                success = false;
+            }
+            if (success && !properties.containsKey("http.proxyPassword")) {
+                final char[] password = System.console().readPassword("Please type in your proxy password: ");
+                properties.setProperty("http.proxyPassword", new String(password));
+                properties.setProperty("https.proxyPassword", new String(password));
+            }
+            properties.forEach((k, v) -> System.setProperty(k.toString(), v.toString()));
+        }
+        return properties;
     }
 }
 //     static class Tweet {
